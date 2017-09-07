5ac5f1a5482e11a7fb250eafd587f05074bbeffd
Bredgren <brandon.edgren@gmail.com>
2017-09-06 19:48:06 -0700

    Add doc comments to sprite types/functions

--- game/sprite/sheet.go
+++ game/sprite/sheet.go
@@ -2,9 +2,14 @@ package sprite
 
 import "github.com/hajimehoshi/ebiten"
 
+// SheetDesc maps from sprite name to a description of that sprite.
 type SheetDesc map[string]Desc
+
+// Desc is a list of frames that make up a sprite.
 type Desc []FrameDesc
 
+// FrameDesc describes the position and size of a single frame. When drawing a sprite the
+// anchor for each frame will be put at the position the sprite is drawn at.
 type FrameDesc struct {
 	X      int `json:"x"`
 	Y      int `json:"y"`
@@ -16,5 +21,10 @@ type FrameDesc struct {
 	} `json:"anchor"`
 }
 
-func NewSheet(img *ebiten.Image, layout SheetDesc) {
+// AddSheet extracts sprites from the sheet according to the given layout. Sprites may
+// be retrieved afterward via the Get function. Sprite names (the keys of SheetDesc) must
+// be unique among all sheets added with this function. A non-nil error is returned if
+// this is not the case.
+func AddSheet(img *ebiten.Image, layout SheetDesc) error {
+	return nil
 }
---
+++ game/sprite/sprite.go
@@ -0,0 +1,6 @@
+package sprite
+
+// Get returns the sprite with the give name.
+func Get(name string) {
+	// return a sprite
+}
